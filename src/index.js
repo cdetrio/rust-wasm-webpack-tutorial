3a04ec0bd67066a5b46ce52ce82afe674d6068fd
cdetrio <cdetrio@gmail.com>
2017-08-14 10:24:28 -0400

    wasm elliptic curve pairing example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const wasm = require('./main.rs')
 
-wasm.initialize().then(module => {
+wasm.initialize({'noExitRuntime': true}).then(module => {
+  // Create a Javascript wrapper around our Rust function
   const add = module.cwrap('add', 'number', ['number', 'number'])
+  
+  const bn128 = module.cwrap('bn128', 'number', [])
+  
   console.log('Calling rust functions from javascript!')
   console.log(add(1, 2))
+  
+  console.log('diong ecadd and ecmul.')
+  console.log(bn128())
 })
